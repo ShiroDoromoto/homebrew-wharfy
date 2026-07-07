200891006d6cdf7798ff57858dcdb4d11c96823d
ShiroDoro <ShiroDoromoto@users.noreply.github.com>
2026-07-07 23:47:43 +0900

    wharfy: update wharfy 0.14.0

--- Formula/wharfy.rb
+++ Formula/wharfy.rb
@@ -1,27 +1,27 @@
 class Wharfy < Formula
   homepage "https://github.com/ShiroDoromoto/wharfy"
-  version "0.13.0"
+  version "0.14.0"
   license "AGPL-3.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/ShiroDoromoto/wharfy/releases/download/v0.13.0/wharfy_0.13.0_darwin_arm64.tar.gz"
-      sha256 "b436fb952804111b7ae03d63d85f554a11a940250c8dd5573134bfd5823e8fd8"
+      url "https://github.com/ShiroDoromoto/wharfy/releases/download/v0.14.0/wharfy_0.14.0_darwin_arm64.tar.gz"
+      sha256 "d1b8a0d2efe6571805cacc9774cf596873e728e479e50ada5b384df97005e43c"
     end
     on_intel do
-      url "https://github.com/ShiroDoromoto/wharfy/releases/download/v0.13.0/wharfy_0.13.0_darwin_amd64.tar.gz"
-      sha256 "fabbd6868eba11d28065800f94076ff0535040133d68393bfe3dadce58f77369"
+      url "https://github.com/ShiroDoromoto/wharfy/releases/download/v0.14.0/wharfy_0.14.0_darwin_amd64.tar.gz"
+      sha256 "a18f7f87b5a62f64b30a0a51e8de1e07802804184eef547b898c8a012f7e27f9"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/ShiroDoromoto/wharfy/releases/download/v0.13.0/wharfy_0.13.0_linux_arm64.tar.gz"
-      sha256 "6e955b3d532babff6ee459a39155c17645aacae0a4285e34a88c6dee75ccded3"
+      url "https://github.com/ShiroDoromoto/wharfy/releases/download/v0.14.0/wharfy_0.14.0_linux_arm64.tar.gz"
+      sha256 "92526b005254bd86d10d1c6329d53370fe93b267c86491474cc1550b5efa67ae"
     end
     on_intel do
-      url "https://github.com/ShiroDoromoto/wharfy/releases/download/v0.13.0/wharfy_0.13.0_linux_amd64.tar.gz"
-      sha256 "6dad69b461a7ac044766bb81e8ed789ddc933c84e484f656fedb4575a5f1dfde"
+      url "https://github.com/ShiroDoromoto/wharfy/releases/download/v0.14.0/wharfy_0.14.0_linux_amd64.tar.gz"
+      sha256 "84a5bf66dd954a1e8ee2ef591fbd6a6ae655cac6323ba7217df01811682e049a"
     end
   end
 
@@ -30,6 +30,6 @@ class Wharfy < Formula
   end
 
   test do
-    assert_match "0.13.0", shell_output("#{bin}/wharfy version")
+    assert_match "0.14.0", shell_output("#{bin}/wharfy version")
   end
 end
